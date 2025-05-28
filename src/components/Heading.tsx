d757f7b1d6eafdc51a8a7f4057f2c6c502e77927
Boti <medgyesi.me@icloud.com>
2025-05-28 11:32:22 +0200

    created the header componenet

---
+++ src/components/Heading.tsx
@@ -0,0 +1,39 @@
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { createElement } from "react";
+
+const tabs = ["Home", "About", "Services", "Contact"];
+
+const Heading = () => {
+  return (
+    <div className="bg-dark d-flex">
+      <i
+        className="bi bi-calendar4-week m-3"
+        style={{ fontSize: "2rem", color: "white" }}
+      ></i>
+      {tabs.map((tab) => (
+        <a
+          key={tab}
+          className="text-white my-auto mx-2 link-underline-dark"
+          href="#"
+        >
+          {tab}
+        </a>
+      ))}
+
+      <div className="d-flex my-auto ms-auto">
+        <i
+          className="bi bi-person-circle"
+          style={{ color: "white" }}
+        ></i>
+        <a
+          className="text-white link-underline-dark mx-2"
+          href="#"
+        >
+          Demo
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Heading;
