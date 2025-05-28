b68ae1abb448d07dcb5e0ab1efa2104c5c15a5f7
Boti <medgyesi.me@icloud.com>
2025-05-28 16:06:00 +0200

    finnished dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,11 +1,64 @@
-
-
 const Dashboard = () => {
   return (
-    <div>
-      <h1>Dashboard Body</h1>
+    <div className="m-3 w-100">
+      <h1 className="my-3">Dashboard Body</h1>
+      <form>
+        <div className="form-group my-1">
+          <label htmlFor="title">Meeting Title</label>
+          <input
+            type="text"
+            className="form-control"
+            id="meetingTitle"
+            aria-describedby="emailHelp"
+            placeholder="Enter meeting title"
+          />
+        </div>
+        <div className="d-flex my-1">
+          <div className="form-group w-50 me-4">
+            <label htmlFor="meetingDate">Meeting Date</label>
+            <input
+              type="date"
+              className="form-control"
+              id="date"
+              placeholder="dd/mm/yyyy"
+            />
+          </div>
+          <div className="form-group w-50">
+            <label htmlFor="meetingTime">Meeting Time</label>
+            <input
+              type="time"
+              className="form-control"
+              id="time"
+              placeholder="--:--"
+            />
+          </div>
+        </div>
+        <div className="form-group my-1">
+          <label htmlFor="meetingLevel">Meeting Level</label>
+          <select className="form-control" id="level">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+        </div>
+        <div className="form-group my-1">
+          <label htmlFor="participants">Meeting Participants</label>
+          <textarea
+            className="form-control"
+            id="participants"
+            placeholder="Enter meeting participants"
+            rows={3}
+          ></textarea>
+        </div>
+        <button type="button" className="btn btn-primary">
+          <i className="bi bi-calendar-plus-fill me-2"></i>
+          Create Meeting
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,10 +1,10 @@
-import Dashboard from "./Dashboard";
+
 
 const dashItems = [
   { id: 1, title: " Dashboard", className: "bi bi-speedometer list-group-item mx-1 " }, // bi
-  { id: 2, title: " Meetings", className: "bi bi-calendar2-plus list-group-item mx-1" },
-  { id: 3, title: " Users", className: "bi bi-people list-group-item mx-1" },
-  { id: 4, title: " Calendar", className: "bi bi-calendar4-week list-group-item mx-1" },
+  { id: 2, title: " Meetings", className: "bi bi-webcam-fill list-group-item mx-1" },
+  { id: 3, title: " Users", className: "bi bi-people-fill list-group-item mx-1" },
+  { id: 4, title: " Calendar", className: "bi bi-calendar-day-fill list-group-item mx-1" },
 ];
 
 const Menu = () => {
