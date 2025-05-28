c91bd26d273345c1572ff20f474fc1874632251b
Boti <medgyesi.me@icloud.com>
2025-05-28 13:30:33 +0200

    adjusted menu style

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,22 +1,20 @@
 import Dashboard from "./Dashboard";
 
 const dashItems = [
-  { id: 1, title: "Dashboard", className: "bi bi-speedometer" }, // bi
-  { id: 2, title: "Meetings", className: "bi bi-calendar2-plus" },
-  { id: 3, title: "Users", className: "bi bi-people" },
-  { id: 4, title: "Calendar", className: "bi bi-calendar4-week" },
+  { id: 1, title: " Dashboard", className: "bi bi-speedometer list-group-item mx-1 " }, // bi
+  { id: 2, title: " Meetings", className: "bi bi-calendar2-plus list-group-item mx-1" },
+  { id: 3, title: " Users", className: "bi bi-people list-group-item mx-1" },
+  { id: 4, title: " Calendar", className: "bi bi-calendar4-week list-group-item mx-1" },
 ];
 
 const Menu = () => {
   return (
-    <div style={{ fontSize: "1.25rem", background: "lightgray", width: "250px"}}>
+    <div
+      style={{ fontSize: "1.25rem", width: "250px" }}
+    >
       <ul className="list-group">
         {dashItems.map((item) => (
-          <li
-            key={item.id}
-            className={item.className}
-            
-          >
+          <li key={item.id} className={item.className}>
             {item.title}
           </li>
         ))}
