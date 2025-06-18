22319d7a2c1dea3ea0061c51da49319f500d0249
Boti <medgyesi.me@icloud.com>
2025-06-18 13:21:36 +0200

    added swagger config, corrected pom.xml and added meeting entity

---
+++ Backend/src/main/java/com/springboot/backend/entity/Meeting.java
@@ -0,0 +1,33 @@
+package com.springboot.backend.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Time;
+import java.util.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+public class Meeting {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private Date date;
+    @Column(nullable = false)
+    private Time time;
+    @Column
+    private String priority;
+    private String participants;
+
+}
